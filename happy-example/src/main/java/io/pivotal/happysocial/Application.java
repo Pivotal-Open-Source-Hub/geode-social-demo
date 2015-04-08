a01789ea8a0d7884112ac2f76dea4646686c8429
Dan Smith <dsmith@pivotal.io>
2015-04-07 18:19:32 -0700

    A few minor changes to example code.

--- happy-example/src/main/java/io/pivotal/happysocial/Application.java
+++ happy-example/src/main/java/io/pivotal/happysocial/Application.java
@@ -44,48 +44,19 @@ public class Application {
   public void run() {
     people.save(new Person("SpongeBob", "SpongeBob SquarePants"));
     people.save(new Person("Neil", "Neil deGrasse Tyson"));
+    people.save(new Person("Ackbar", "Admiral Ackbar"));
     
     posts.save(new Post("SpongeBob", "Run Mr. Krabs! Run like you’re not in a coma!"));
     posts.save(new Post("Spongebob", "Excuse me, sir, but you’re sitting on my body, which is also my face."));
     posts.save(new Post("Neil", "Space exploration is a force of nature unto itself that no other force in society can rival."));
+    posts.save(new Post("Ackbar", "It's a trap!"));
     
     Collection<Post> neilsPosts = posts.findPosts("Neil");
     System.out.println(neilsPosts);
     
     Set<String> filter = new HashSet<String>();
-    filter.add("Neil");
-    filter.add("SpongeBob");
+    filter.add("Ackbar");
     List<MoodResult> result = functionClient.getMood(filter);
-    System.out.println(result);
-    
-    
-
-//    ClientCache cache = new ClientCacheFactory().addPoolLocator("locahost",
-//        10334).create();
-//
-//    Region<String, Person> people = cache.<String, Person>createClientRegionFactory(PROXY)
-//        .create("people");
-//    
-//    Region<PostId, Post> posts = cache.<PostId, Post>createClientRegionFactory(PROXY)
-//        .create("people");
-//    
-//    people.put("Luke", new Person("Luke"));
-//    people.put("Leia", new Person("Leia"));
-//    people.put("Han", new Person("Han"));
-//    
-//    posts.put(new PostId("Han"), new Post("Han", "Uh, uh... negative, negative. We had a reactor leak here now. Give us a few minutes to lock it down. Large leak, very dangerous."));
-//    posts.put(new PostId("Luke"), new Post("Luke", "If there's a bright center to the universe, you're on the planet that it's farthest from."));
-//    posts.put(new PostId("Han"), new Post("Han", "Look, Your Worshipfulness, let's get one thing straight. I take orders from just one person: me. "));
-//    
-//    
-//    SelectResults<Post> results = posts
-//        .query("select * from posts where person='Han'");
-//    System.out.println(results);
-//
-//    ResultCollector<MoodResult, MoodResult> rc = (ResultCollector<MoodResult, MoodResult>) FunctionService
-//        .onRegion(posts).withFilter(Collections.singleton("Han"))
-//        .execute("getMood");
-//    MoodResult mood = rc.getResult();
-//    System.out.println("Han is feeling " + mood);
+    System.out.println("Admiral Ackbar is being " + result.get(0).getMood());
   }
 }
