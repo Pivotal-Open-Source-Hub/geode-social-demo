6f3776465168f6d9fd1d5f9a6b6c9fe9e189144d
Dan Smith <dsmith@pivotal.io>
2015-04-09 13:33:02 -0700

    Organizing setenv.sh

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -2,10 +2,11 @@
 
 #export JAVA_HOME=/home/dan/software/jdk1.8.0_40
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-export PATH=$JAVA_HOME/bin:$PATH
-export PROJECT_HOME=/home/dsmith/data/apachecon/talk/mydemo/project
+
 #export PROJECT_HOME=/home/dan/MyStuff/Code/geode-demo
+export PROJECT_HOME=/home/dsmith/data/apachecon/talk/mydemo/project
+
+
 export GEMFIRE_HOME=$PROJECT_HOME/geode/gemfire-assembly/build/install/geode/
-
-
+export PATH=$JAVA_HOME/bin:$PATH
 export PATH=$PATH:$GEMFIRE_HOME/bin
