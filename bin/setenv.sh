7c11cc9e7e22c49bb0df0aa34a191c6695003702
Dan Smith <upthewaterspout@gmail.com>
2015-04-08 22:07:40 -0700

    Putting absolute paths in setenv.sh
    Some absolute paths had crept into some of the other startup scripts.

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-export GEMFIRE_HOME=/home/dsmith/data/apachecon/talk/mydemo/project/geode/gemfire-assembly/build/install/geode/
+
+#export JAVA_HOME=/home/dan/software/jdk1.8.0_40
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export PATH=$JAVA_HOME/bin:$PATH
+export PROJECT_HOME=/home/dsmith/data/apachecon/talk/mydemo/project
+#export PROJECT_HOME=/home/dan/MyStuff/Code/geode-demo
+export GEMFIRE_HOME=$PROJECT_HOME/geode/gemfire-assembly/build/install/geode/
 
 
 export PATH=$PATH:$GEMFIRE_HOME/bin
