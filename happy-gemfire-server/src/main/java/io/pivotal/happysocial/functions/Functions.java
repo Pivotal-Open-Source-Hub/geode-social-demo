1184adbf6f3f01ba80505b277023e2c43a55c7f5
Dan Smith <dsmith@pivotal.io>
2015-04-10 16:20:15 -0700

    Removing some unused imports

--- happy-gemfire-server/src/main/java/io/pivotal/happysocial/functions/Functions.java
+++ happy-gemfire-server/src/main/java/io/pivotal/happysocial/functions/Functions.java
@@ -3,18 +3,14 @@ package io.pivotal.happysocial.functions;
 import io.pivotal.happysocial.model.Post;
 import io.pivotal.happysocial.model.PostId;
 import io.pivotal.happysocial.model.SentimentResult;
-import io.pivotal.happysocial.repositories.PostRepository;
 import io.pivotal.happysocial.sentiment.SentimentAnalyzer;
 
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.gemfire.function.annotation.Filter;
 import org.springframework.data.gemfire.function.annotation.GemfireFunction;
-import org.springframework.data.gemfire.mapping.GemfireMappingContext;
-import org.springframework.data.gemfire.repository.support.GemfireRepositoryFactory;
 import org.springframework.stereotype.Component;
 
 import com.gemstone.gemfire.cache.Region;
@@ -25,11 +21,11 @@ public class Functions {
   SentimentAnalyzer sentimentAnalyzer;
 
   @GemfireFunction(HA=true)
-  public SentimentResult getSentiment(Region<PostId, Post> localPosts, @Filter Set<String> personNames) throws Exception {
+  public SentimentResult getSentiment(Region<PostId, Post> localPosts, 
+      @Filter Set<String> personNames) throws Exception {
     String personName = personNames.iterator().next();
     Collection<Post> posts = localPosts.query("id.person='" + personName + "'");
     String sentiment = sentimentAnalyzer.analyze(posts);
     return new SentimentResult(sentiment, personName);
-    
   }
 }
